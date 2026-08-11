140844e7a08c0247ab43fc67519c3e4cb5c9dc78
Matt <profito.systems@gmail.com>
2026-08-11 21:15:04 +0100

    Sync the production Miarka implementation

--- src/cv/warp.js
+++ src/cv/warp.js
@@ -1,28 +1,29 @@
 function warpToA4(inputCanvas, corners, warpedCanvas) {
   const src = cv.imread(inputCanvas);
   const dst = new cv.Mat();
-
-  const srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, [
+  const srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, Array.of(
     corners[0].x, corners[0].y,
     corners[1].x, corners[1].y,
     corners[2].x, corners[2].y,
-    corners[3].x, corners[3].y
-  ]);
+    corners[3].x, corners[3].y,
+  ));
 
-  const w = warpedCanvas.width;
-  const h = warpedCanvas.height;
-
-  const dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, [
+  const width = warpedCanvas.width;
+  const height = warpedCanvas.height;
+  const dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, Array.of(
     0, 0,
-    w, 0,
-    w, h,
-    0, h
-  ]);
+    width - 1, 0,
+    width - 1, height - 1,
+    0, height - 1,
+  ));
 
-  const M = cv.getPerspectiveTransform(srcTri, dstTri);
-  cv.warpPerspective(src, dst, M, new cv.Size(w, h));
-
+  const transform = cv.getPerspectiveTransform(srcTri, dstTri);
+  cv.warpPerspective(src, dst, transform, new cv.Size(width, height));
   cv.imshow(warpedCanvas, dst);
 
-  srcTri.delete(); dstTri.delete(); M.delete(); src.delete(); dst.delete();
+  srcTri.delete();
+  dstTri.delete();
+  transform.delete();
+  src.delete();
+  dst.delete();
 }
